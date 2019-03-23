b19298037118858449c7a05b4ef9a7d02201a46c
fran-man <fran.man@hotmail.co.uk>
2019-03-23 06:31:07 +0000

    Make the grid 2d
    Reset still does not work properly

--- src/main/java/com/franm/randomwalk/Coordinates/CoordinateHelper.java
+++ src/main/java/com/franm/randomwalk/Coordinates/CoordinateHelper.java
@@ -1,5 +1,7 @@
 package com.franm.randomwalk.Coordinates;
 
+import java.util.Arrays;
+import java.util.List;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
@@ -11,7 +13,10 @@ public class CoordinateHelper {
   Logger logger = LoggerFactory.getLogger(CoordinateHelper.class);
 
   private int x = 5;
-  private final int start = 5; //Middle
+  private final int startx = 5; //Middle
+
+  private int y = 5;
+  private final int starty = 5; //Middle
 
   private final Random ran = new Random();
 
@@ -19,10 +24,23 @@ public class CoordinateHelper {
 
   }
 
-  public int IncrementCoordinates(){
-    int returnVal = (this.ran.nextInt(2) % 2 == 0) ? ++x : --x;
-    logger.debug("x = {}", returnVal);
-    return returnVal;
+  public List<Integer> IncrementCoordinates() {
+    int nextRandom = this.ran.nextInt(4);
+    switch (nextRandom) {
+    case 0:
+      this.x++;
+      break;
+    case 1:
+      this.x--;
+      break;
+    case 2:
+      this.y++;
+      break;
+    case 3:
+      this.y--;
+      break;
+    }
+    return Arrays.asList(this.x,this.y);
   }
 
   public int getXCoord(){
@@ -32,6 +50,7 @@ public class CoordinateHelper {
 
   public void ResetCoords(){
     this.logger.info("Resetting...");
-    this.x = this.start;
+    this.x = this.startx;
+    this.y = this.starty;
   }
 }
--- src/main/java/com/franm/randomwalk/Walk/GridGenerator.java
+++ src/main/java/com/franm/randomwalk/Walk/GridGenerator.java
@@ -7,29 +7,39 @@ import org.springframework.stereotype.Component;
 public class GridGenerator {
 
   @Value("${grid.width:9}")
-  private int size;
+  private int width;
+
+  @Value("${grid.height:9}")
+  private int height;
 
   public GridGenerator(){
 
   }
 
-  public String GenerateGridFromCoordinates(int xCoord){
-    StringBuilder builder = (xCoord == -1 ? new StringBuilder("+") : new StringBuilder("|"));
+  public String GenerateGridFromCoordinates(int xCoord, int yCoord){
+    StringBuilder builder = new StringBuilder("");
 
-    for(int i = 0; i < this.size; i++){
-      builder.append(i == xCoord ? "+" : "-");
-    }
-
-    if(xCoord == this.size){
-      builder.append("+");
-    }
-    else{
-      builder.append("|");
+    for(int j = -1; j <= this.height; j++){
+      for(int i = -1; i <= this.width; i++){
+        if(xCoord == i && yCoord == j){
+          builder.append("+");
+        }
+        else if(i == -1 || i == this.width){
+          builder.append("|");
+        }
+        else if(j == -1 || j == this.height){
+          builder.append("-");
+        }
+        else{
+          builder.append(".");
+        }
+      }
+      builder.append(System.getProperty("line.separator"));
     }
     return builder.toString();
   }
 
   public boolean isGameWon(int xCoord){
-    return xCoord == -1 || xCoord == this.size;
+    return xCoord == -1 || xCoord == this.width;
   }
 }
