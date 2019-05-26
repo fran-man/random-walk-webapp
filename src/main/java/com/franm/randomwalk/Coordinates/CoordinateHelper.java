70f03376877963c299b0a09f13697c1c411db815
fran-man <fran.man@hotmail.co.uk>
2019-05-26 12:13:31 +0100

    Consider configuration when setting start point

--- src/main/java/com/franm/randomwalk/Coordinates/CoordinateHelper.java
+++ src/main/java/com/franm/randomwalk/Coordinates/CoordinateHelper.java
@@ -1,5 +1,7 @@
 package com.franm.randomwalk.Coordinates;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Arrays;
 import java.util.List;
 import org.slf4j.LoggerFactory;
@@ -13,15 +15,32 @@ public class CoordinateHelper {
   Logger logger = LoggerFactory.getLogger(CoordinateHelper.class);
 
   private int x = 5;
-  private final int startx = 5; //Middle
+  private int startx = 5; //Middle
 
   private int y = 5;
-  private final int starty = 5; //Middle
+  private int starty = 5; //Middle
 
   private final Random ran = new Random();
 
-  public CoordinateHelper(){
+  @Autowired
+  public CoordinateHelper(@Value("${grid.width:9}") final int width, @Value("${grid.height:9}") final int height){
+    if(width % 2 == 0){
+      this.startx = width/2;
+      this.x = width/2;
+    }
+    else{
+      this.startx = (width - 1)/2;
+      this.x = (width - 1)/2;
+    }
 
+    if(height % 2 == 0){
+      this.starty = height/2;
+      this.y = height/2;
+    }
+    else{
+      this.starty = (height - 1)/2;
+      this.y = (height - 1)/2;
+    }
   }
 
   public List<Integer> IncrementCoordinates() {
